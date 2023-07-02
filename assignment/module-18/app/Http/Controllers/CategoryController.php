805c921007494e82239e8b229314d90ee42e5df2
bablu22 <mdbablu22.bablu22@gmail.com>
2023-07-02 21:59:09 +0600

    assignment-18 done

---
+++ assignment/module-18/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function getPostsByCategory($id)
+    {
+        $category = Category::findOrFail($id);
+        $posts = $category->posts;
+
+        return $posts;
+    }
+    public function latestPostWithCategory($categoryId)
+    {
+        $category = Category::find($categoryId);
+        $categories = $category->latestPost();
+
+        return view('latestPost', compact('categories'));
+    }
+}
---
+++ assignment/module-18/app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::with('category')->get();
+        $allPosts = Post::all();
+
+        return view('posts', compact('posts'));
+    }
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        return "Post with ID $id has been deleted.";
+    }
+}
